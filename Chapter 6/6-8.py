6611e43d2f49a6fc45c2d212999ac8b8df84b263
Z0th3n <vicente.barcelo@hotmail.es>
2021-02-23 00:57:48 +0100

    Ejercicios

---
+++ Chapter 6/6-8.py
@@ -0,0 +1,16 @@
+# Pets exercise
+kora = {
+    'raza': 'bretón',
+    'dueño': 'perico',
+    'edad': 2,
+    }
+teddy = {
+    'raza': 'yorkshire',
+    'dueño': 'laura',
+    'edad': 7,
+    }
+
+pets = [kora, teddy]
+
+for pet in pets:
+    print(pet)
